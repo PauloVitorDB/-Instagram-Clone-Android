3e622d537c1c3d890705f509c97d35e83c19ca63
Paulo Vitor <73504550+PauloVitorDB@users.noreply.github.com>
2022-01-16 16:40:54 -0300

    Profile picture upload screen

---
+++ app/src/main/java/co/pvitor/instagram/view/login/RegisterPhotoFragment.kt
@@ -0,0 +1,52 @@
+package co.pvitor.instagram.view.login
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import co.pvitor.instagram.R
+import co.pvitor.instagram.common.view.CustomDialog
+import co.pvitor.instagram.databinding.FragmentRegisterPhotoBinding
+
+class RegisterPhotoFragment: Fragment() {
+
+    private lateinit var binding: FragmentRegisterPhotoBinding
+    private lateinit var dialog: CustomDialog
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) : View {
+        return inflater.inflate(R.layout.fragment_register_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRegisterPhotoBinding.bind(view)
+
+        dialog = CustomDialog(requireContext())
+
+        binding.imageViewAddPhoto.setOnClickListener(addPhotoListener)
+        binding.loadingButtonAddPhoto.setOnClickListener(addPhotoListener)
+    }
+
+    private val addPhotoListener = View.OnClickListener {
+
+        dialog.addTextButton(
+            R.string.galery,
+            R.string.camera
+        ) {
+            when(it.id) {
+                R.string.galery ->  Log.d("buttonListener", "Galeria")
+                R.string.camera -> Log.d("buttonListener", "Cameria")
+            }
+            dialog.dismiss()
+        }
+        dialog.setTitle(getText(R.string.add_profile_photo))
+        dialog.show()
+    }
+}
