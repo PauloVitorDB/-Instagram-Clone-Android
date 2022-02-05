d9247556e4ae4f9dc196f14d4fce0b9d5f88ebef
paulo <pvitor.db@gmail.com>
2022-02-04 23:32:22 -0300

    Base fragment by inheritance, using generics

---
+++ app/src/main/java/co/pvitor/instagram/common/base/BaseFragment.kt
@@ -0,0 +1,69 @@
+package co.pvitor.instagram.common.base
+
+import android.os.Bundle
+import android.view.*
+import androidx.annotation.LayoutRes
+import androidx.annotation.MenuRes
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<P: BasePresenter, B>(
+    @LayoutRes private val layoutId: Int,
+    private val bind: (View) -> B
+): Fragment() {
+
+    private var _binding: B? = null
+    protected val binding get() = _binding!!
+
+    abstract var presenter: P
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+        getMenu()?.let {
+            inflater.inflate(it, menu)
+        }
+
+        setupPresenter()
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    @MenuRes
+    open fun getMenu(): Int? {
+        return null
+    }
+
+    abstract fun setupPresenter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(layoutId, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = bind(view)
+
+        setupOnViewCreated()
+
+    }
+
+    abstract fun setupOnViewCreated()
+
+    override fun onDestroy() {
+        presenter.onDestroy()
+        _binding = null
+        super.onDestroy()
+    }
+
+
+}
